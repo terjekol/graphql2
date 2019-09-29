3a50f9c23f708fbcf2940c025fa6ef65c7135343
Terje Kolderup <terje@kolderup.net>
2019-09-29 15:54:09 +0200

    x

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,48 +1,20 @@
 import React, { Component } from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { Route } from 'react-router';
-import { Home } from './components/Home';
-import { Layout } from './components/Layout';
-import FetchData from './components/FetchData';
-import { Counter } from './components/Counter';
+import Layout from './components/Layout';
 import { BrowserRouter } from 'react-router-dom';
-import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
-//import { gql } from 'apollo-boost';
-
-const client = new ApolloClient({ uri: '/graphql' });
-// client
-//   .query({
-//     query: gql`{
-//       author(id: 1) {
-//         name,
-//         books {
-//           id, name
-//         }
-//       }
-//     }`
-//   })
-//   .then(result => console.log(result));
+import { Security } from '@okta/okta-react';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 
 export default class App extends Component {
-
   render() {
     return (
       <BrowserRouter basename={baseUrl}>
         <Security issuer='https://dev-660868.okta.com/oauth2/default'
           clientId='0oa1ge0utjlVWYpSX357'
           redirectUri={window.location.origin + '/implicit/callback'}
-          pkce={true} >
-          <ApolloProvider client={client}>
-            <Layout>
-              <Route exact path='/' component={Home} />
-              <Route path='/counter' component={Counter} />
-              <SecureRoute path='/fetch-data' component={FetchData} />
-              <Route path='/implicit/callback' component={ImplicitCallback} />
-            </Layout>
-          </ApolloProvider>
+          pkce={true}>
+          <Layout>
+          </Layout>
         </Security>
       </BrowserRouter>
     );
--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
+import ApolloClient from 'apollo-boost';
 import NavMenu from './NavMenu';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { Route } from 'react-router';
+import { Home } from './Home';
+import { SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import { Counter } from './Counter';
+import FetchData from './FetchData';
 
-export class Layout extends Component {
-  static displayName = Layout.name;
+import withAuth from '@okta/okta-react/dist/withAuth';
 
-  render () {
+class Layout extends Component {
+
+  render() {
+
+    const clientParam = { uri: '/graphql' };
+    let myAuth = this.props && this.props.auth;
+    console.log(this.props);
+    console.log('myAuth', myAuth);
+    if (myAuth) {
+      clientParam.request = async (operation) => {
+        let token = await myAuth.getAccessToken();
+        console.log(token);
+        operation.setContext({ headers: { authorization: token ? `Bearer ${token}` : '' } });
+      }
+    }
+    const client = new ApolloClient(clientParam);
+
     return (
       <div>
         <NavMenu />
         <Container>
-          {this.props.children}
+          <ApolloProvider client={client} >
+            <Route exact path='/' component={Home} />
+            <Route path='/counter' component={Counter} />
+            <SecureRoute path='/fetch-data' component={FetchData} />
+            <Route path='/implicit/callback' component={ImplicitCallback} />
+          </ApolloProvider>
         </Container>
       </div>
     );
   }
 }
+
+export default withAuth(Layout);
